fd131dc38a1838fb6da432c05b83bd261a484567
Arosh Jayamanna <70162636+aroshjayamanne@users.noreply.github.com>
2023-01-19 14:25:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //hide the form when the page is loaded
 const btn = document.querySelector(".btn-opn");
 const form = document.querySelector(".fact-form");
+
 btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
+    btn.textContent = "Close";
   } else {
     form.classList.add("hidden");
+    btn.textContent = "Share a Fact";
   }
 });
